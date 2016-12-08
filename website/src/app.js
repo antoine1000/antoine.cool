0ceaa364d510df2af8f0ae2eb5d5f99672e10433
antoine1000 <bonjour@antoine.cool>
2016-12-08 17:34:21 +0100

    project page v0.2

--- website/src/app.js
+++ website/src/app.js
@@ -1,1 +1,10 @@
 console.log(`It's working !`)
+
+//const jquery = require('jquery');
+// const stickyKit = require('sticky-kit');
+
+
+/* ----- StickyKit ----- */
+
+$(".title").stick_in_parent("html");
+
