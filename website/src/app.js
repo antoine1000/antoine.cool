d12c6b453b57106198c22e07399085d027fa6383
antoine1000 <bonjour@antoine.cool>
2016-12-08 18:35:32 +0100

    fix sticky nav arrows
    works on firefox, chrome, safari

--- website/src/app.js
+++ website/src/app.js
@@ -1,10 +1,11 @@
 console.log(`It's working !`)
 
-//const jquery = require('jquery');
-// const stickyKit = require('sticky-kit');
+var stickyElements = document.getElementsByClassName('sticky');
 
+for (var i = stickyElements.length - 1; i >= 0; i--) {
+    Stickyfill.add(stickyElements[i]);
+}
 
-/* ----- StickyKit ----- */
+//$('.sticky').Stickyfill();
 
-$(".title").stick_in_parent("html");
 
