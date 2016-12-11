a3e5270f605886684cf301f2d270f3177b6628ab
pqml <hugo.piquemal@gmail.com>
2016-12-11 18:53:26 +0100

    Add dynamic project listing

--- website/config/brostart.config.js
+++ website/config/brostart.config.js
@@ -1,3 +1,10 @@
+const projectListSync = require('../scripts/plugins/projectListSync')
+const path = require('path')
+const paths = require('./paths.config.js')
+const projectOrder = require('./projects.config.js')
+
+let projectList
+
 const broStartConfig = {
   devServer: {
     historyAPIFallback: true,
@@ -12,6 +19,12 @@ const broStartConfig = {
   },
   lifecycle: {
     onHandlebarsInit (handlebars) {
+      // load the project list
+      projectList = projectListSync(
+        path.join(paths.content, 'projets'),
+        projectOrder,
+        ['projet', 'date'])
+
       // add your own custom helpers, partials, decorators here
       handlebars.registerHelper('nl2br', (text) => {
         const reg = /([^>\r\n]?)(\r\n|\n\r|\r|\n)/g
@@ -21,6 +34,11 @@ const broStartConfig = {
     },
     beforeHandlebarsRender (data) {
       // you can mutate data if you need to add/remove content before rendering
+      if (data.layout === 'index.hbs') data.project = projectList.projects
+      if (data.layout === 'projet.hbs') {
+        data.nextProject = projectList.previousProjectFrom(data.projet)
+        data.previousProject = projectList.nextProjectFrom(data.projet)
+      }
     }
   }
 }
---
+++ website/config/projects.config.js
@@ -0,0 +1,11 @@
+module.exports = [
+  `eyebro`,
+  `skeleton+kinect`,
+  `one thing leading to another`,
+  `dusangneuf-osangsett`,
+  `laurent de la house`,
+  `whistleblower`,
+  `j'ai du bon data`,
+  `switch`,
+  `iso-iso`
+]
