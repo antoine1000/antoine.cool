07b93e508996f231fd4d26d1e6d0e799d21d781e
antoine1000 <bonjour@antoine.cool>
2016-12-13 03:12:34 +0100

    add some more content + flickity

--- website/src/app.js
+++ website/src/app.js
@@ -1,11 +1,19 @@
 console.log(`It's working !`)
 
+
 var stickyElements = document.getElementsByClassName('sticky');
 
 for (var i = stickyElements.length - 1; i >= 0; i--) {
     Stickyfill.add(stickyElements[i]);
 }
 
-//$('.sticky').Stickyfill();
+// import flickity module
+const Flickity = require('flickity');
 
+// var elem = document.querySelector('.main-carousel');
+// var flkty = new Flickity( elem, {
+//   // options
+//   cellAlign: 'left',
+//   contain: true
+// });
 
